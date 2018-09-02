d0125253ced492c0a72d8e3c4d7179ffaa37b06c
Fernando Paredes García <fernando@develcuy.com>
2018-09-02 00:47:56 -0500

    Fix typo

--- src/crypter.cpp
+++ src/crypter.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2012 The Bitcoin developers
-// Copyright (c) 2009-2018 The Devcoin Developers
+// Copyright (c) 2009-2018 The Devcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
