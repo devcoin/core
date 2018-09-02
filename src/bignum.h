24f2889cd40045cfc38e25b582cf9cef7b344d16
Fernando Paredes García <fernando@develcuy.com>
2018-09-02 00:37:50 -0500

    Restore function getuint64

--- src/bignum.h
+++ src/bignum.h
@@ -209,6 +209,21 @@ public:
         BN_mpi2bn(pch, p - pch, self);
     }
 
+    uint64 getuint64()
+    {
+        unsigned int nSize = BN_bn2mpi(self, NULL);
+        if (nSize < 4)
+            return 0;
+        std::vector<unsigned char> vch(nSize);
+        BN_bn2mpi(self, &vch[0]);
+        if (vch.size() > 4)
+            vch[4] &= 0x7f;
+        uint64 n = 0;
+        for (unsigned int i = 0, j = vch.size()-1; i < sizeof(n) && j >= 4; i++, j--)
+            ((unsigned char*)&n)[i] = vch[j];
+        return n;
+    }
+
     void setuint256(uint256 n)
     {
         unsigned char pch[sizeof(n) + 6];
