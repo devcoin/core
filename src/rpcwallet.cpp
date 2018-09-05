8a77ade963676c7d978929c692e23bf0e9f8ba22
ctya <ctyaemail@gmail.com>
2018-09-04 18:19:19 -0700

    added current payout round number to getinfo output

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -86,6 +86,7 @@ Value getinfo(const Array& params, bool fHelp)
     if (pwalletMain->IsCrypted())
         obj.push_back(Pair("unlocked_until", (boost::int64_t)nWalletUnlockTime / 1000));
     obj.push_back(Pair("errors",        GetWarnings("statusbar")));
+    obj.push_back(Pair("round",        (int)(floor(nBestHeight / 4000))));
     return obj;
 }
 
