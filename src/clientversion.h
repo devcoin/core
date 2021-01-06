529973bc208e8700f95b2027505d78c6d247123a
Fernando Paredes Garcia <fernando@develcuy.com>
2021-01-05 21:34:51 -0500

    Mark Devcoin release version 0.8.8.0.

--- src/clientversion.h
+++ src/clientversion.h
@@ -8,7 +8,7 @@
 // These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       8
-#define CLIENT_VERSION_REVISION    7
+#define CLIENT_VERSION_REVISION    8
 #define CLIENT_VERSION_BUILD       0
 
 // Set to true for release, false for prerelease or test build
@@ -16,7 +16,7 @@
 
 // Copyright year (2009-this)
 // Todo: update this when changing our copyright comments in the source
-#define COPYRIGHT_YEAR 2020
+#define COPYRIGHT_YEAR 2021
 
 // Converts the parameter X to a string after macro replacement on X has been performed.
 // Don't merge these into one macro!
